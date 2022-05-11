d3521e4b65cf4dbf26cf472863328605d6c368d0
DeIndi <norogozin@mail.com>
2022-05-11 22:23:15 +0300

    linter action update

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
-export function input () 
+
+export default function input()
 {
-    const name = readlineSync.question('May I have your name?');
-    console.log('Hello, ' + name + '!');
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${name}!`);
 }
