bde25db464beeb2af4a60bccfd299b85fe55ca79
NRog <norogozin@gmail.com>
2022-05-19 14:04:14 +0400

    restore point

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,20 +1,20 @@
-import readlineSync from 'readline-sync';
 import { mainLoop } from '../src/index.js';
+
 export const Calc = (name) => {
-	let results = [];
-	let questions = [];
-	for (let i = 0; i < 3; i++) {
-		let n1 = Math.floor(Math.random() * 100);
-		let n2 = Math.floor(Math.random() * 100);
-		const opArray = ['+','-','*'];
-		const op = opArray[Math.floor(Math.random() * opArray.length)];
-		let result = 0;
-		switch (op){
-		  case '+': results[i] = n1 + n2; questions[i] = `${n1} + ${n2}`; break;
-		  case '*': results[i] = n1 * n2; questions[i] = `${n1} * ${n2}`; break;
-		  case '-': results[i] = n1 - n2; questions[i] = `${n1} - ${n2}`; break;
-		  default: results[i] = 0;
-		}
-	  }
+  const results = [];
+  const questions = [];
+  for (let i = 0; i < 3; i += 1) {
+    const n1 = Math.floor(Math.random() * 100);
+    const n2 = Math.floor(Math.random() * 100);
+    const opArray = ['+','-','*'];
+    const op = opArray[Math.floor(Math.random() * opArray.length)];
+    let result = 0;
+    switch (op){
+	case '+': results[i] = n1 + n2; questions[i] = `${n1} + ${n2}`; break;
+	case '*': results[i] = n1 * n2; questions[i] = `${n1} * ${n2}`; break;
+	case '-': results[i] = n1 - n2; questions[i] = `${n1} - ${n2}`; break;
+	default: results[i] = 0;
+	}
+  }
   mainLoop( "", name, questions, results );
 }
---
+++ bin/brain-progression.js
@@ -0,0 +1,16 @@
+import { mainLoop } from '../src/index.js';
+
+function progression () {
+let questionText = 'What number is missing in the progression?';
+let questions = [];
+let correctAnswers = [];
+let pArray = [];
+let hiddenElem = pArray[0];
+let pk = Math.random() * 11;
+for (let j = 0; j < 5; j++) {
+pArray.push (i + pk);
+questions[i] += `${pArray[j]}`;
+}
+hiddenElem = pArray[Math.floor (Math.random() * 6)];
+
+}
