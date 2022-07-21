f9e6144f38efe8fb736e3b130d4dfc8ccb759818
NRog <norogozin@gmail.com>
2022-07-21 18:17:15 +0400

    Fixed brain-prime logic

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -10,7 +10,7 @@ const isPrime = (n) => {
 
 const generateQuestion = () => generateFloorRand(100);
 
-const generateAnswer = (question) => isPrime(parseInt(question, 10) ? 'yes' : 'no');
+const generateAnswer = (question) => isPrime(parseInt(question, 10)) ? 'yes' : 'no';
 
 const startPrimeGame = () => {
   const questionDesc = 'Answer "yes" if given number is prime. Otherwise answer "no".';
